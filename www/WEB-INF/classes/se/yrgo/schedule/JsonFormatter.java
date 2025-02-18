26948c671fa46f8979579fa7a9264cb6157823d3
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-02-18 14:02:48 +0100

    Deluppgift 5. Implementera Json-formateraren

--- www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
+++ www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
@@ -2,25 +2,43 @@ package se.yrgo.schedule;
 
 import java.util.*;
 
+import org.json.*;
+
 /**
  * A class implementing the Formatter interface. Formats a List of Assignment
  * to Json.
  *
  */
 public class JsonFormatter implements Formatter {
+
+  private JSONObject JSONAssignment(Assignment assignment) {
+    JSONObject JSONAssignment = new JSONObject();
+    JSONAssignment.put("date", assignment.date());
+
+    JSONObject JSONSubstitute = new JSONObject();
+    JSONSubstitute.put("name", assignment.teacher().name());
+
+    JSONAssignment.put("substitute", JSONSubstitute);
+
+    JSONObject JSONSchool = new JSONObject();
+    JSONSchool.put("school_name", assignment.school().name());
+    JSONSchool.put("address", assignment.school().address());
+
+    JSONAssignment.put("school", JSONSchool);
+    return JSONAssignment;
+  }
+
   public String format(List<Assignment> assignments) {
-    return "[ { \"some-key\": \"some-value\" } ]";
-    // String jsonString = "";
-    // JSONArray JSON = new JSONArray();
 
-    // if (assignments.size() == 0) {
-    // return "No assignments found";
-    // }
+    if (assignments.size() == 0) {
+      return "[]";
+    } else {
+      JSONArray JSON = new JSONArray();
+      for (Assignment assignment : assignments) {
+        JSON.put(JSONAssignment(assignment));
+      }
+      return JSON.toString(2);
+    }
 
-    // for (Assignment assignment : assignments) {
-    // JSON.put(JSONSchedule(assignment));
-    // }
-
-    // return JSON.toString(2);
   }
 }
